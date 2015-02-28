345250a7ceb784788d1516052c987ab64676bc9c
Jared Radin <jared.radin@gmail.com>
2015-02-28 15:05:39 -0800

    add models

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ function generateViews(sequelize, callback){
 		var sqlQueries = data.toString().split(";");
 		function executeQueries(sqlQueries, index){
 			if(sqlQueries.length <= index){
-//				console.log('Views Created!');
+				//				console.log('Views Created!');
 				return callback();
 			}
 			if(sqlQueries[index].length == 0){
@@ -38,19 +38,48 @@ function generateTables(sequelize, force, cb){
 	// 	file: Sequelize.STRING
 	// });
 
-	mysqlTables.Play = sequelize.define('play_table', {
+	mysqlTables.User = sequelize.define('user', {
+		userName: {type: Sequelize.STRING},
+		realName: {type: Sequelize.STRING}
+		email: {type: Sequelize.STRING}
+		password: {type: Sequelize.STRING},
+	})
+
+	mysqlTables.Play = sequelize.define('play', {
 		startedAt: {type: Sequelize.DATE},
-		finishedAt: {type: sequelize.DATE},
-		gameVersion: {type: sequelize.INTEGER}
-		coinsIn: {type: sequelize.INTEGER},
-		ticketsOut: {type: sequelize.INTEGER},
-		score: {type: sequelize.INTEGER},
-		machineID: {type: sequelize.INTEGER},
-		playerID: {type: sequelize.INTEGER}
+		finishedAt: {type: Sequelize.DATE},
+		gameVersion: {type: Sequelize.STRING}
+		coinsIn: {type: Sequelize.INTEGER},
+		ticketsOut: {type: Sequelize.INTEGER},
+		score: {type: Sequelize.INTEGER}
 	});
 
-	
+	mysqlTables.Arcade = sequelize.define('arcade', {
+		gameTitle: {type: Sequelize.STRING},
+		physicalIdent: {type: Sequelize.STRING},
+		ticketCapacity: {type: Sequelize.INTEGER},
+		coinCapacity: {type: Sequelize.INTEGER},
+		firmwareVer: {type: Sequelize.STRING}
+	});
 
+	mysqlTables.Arcade.hasOne(Play, {as: 'arcade'})
+
+	mysqlTables.Maintenence = sequelize.define('maintenence', {
+		action: {type: Sequelize.STRING},
+		date: {type: Sequelize.DATE},
+		reason: {type: Sequelize.STRING}
+	})
+
+	mysqlTables.Arcade.hasOne(Maintenence, {as: 'arcade'}).
+
+	mysqlTables.Player= sequelize.define('player' {
+		name: {type: Sequelize.STRING},
+		device: {type: Sequelize.STRING},
+	})
+
+	mysqlTables.Play.hasMany(Player, {as: 'plays'})
+
+	
 	// mySQL VIEWs
 	/////////////
 
@@ -60,34 +89,34 @@ function generateTables(sequelize, force, cb){
 	.success(function(){
 		if(force)
 			generateViews(sequelize, cb);
-		else
-		 	cb();
-	})
-	.error(function(error){
-		console.log(error);
-	});
+			else
+				cb();
+			})
+			.error(function(error){
+				console.log(error);
+			});
 
 
-};
+		};
 
-mysqlTables.config = function (cb){
+		mysqlTables.config = function (cb){
 
-	if(!(process.env.CLEARDB_DATABASE_URL))
-		throw 'Invalid database configuration';
+			if(!(process.env.CLEARDB_DATABASE_URL))
+				throw 'Invalid database configuration';
 
-	var forcesync = process.env.DB_FORCE_SYNC ? true : false;
-
-
-	var loginString = process.env.CLEARDB_DATABASE_URL;
-		loginString = (loginString.indexOf("?") !== -1 ? loginString.slice(0, loginString.indexOf("?")) : loginString);
-
-	var sequelizeObj = process.env.LOGDB ? {dialect: 'mysql'} : {dialect: 'mysql', logging: false};
-	var sequelize = new Sequelize(loginString, sequelizeObj);
-	generateTables(sequelize, forcesync, cb);
-
-	return mysqlTables;
-};
+				var forcesync = process.env.DB_FORCE_SYNC ? true : false;
 
 
-//public Database
-module.exports = mysqlTables;
+				var loginString = process.env.CLEARDB_DATABASE_URL;
+				loginString = (loginString.indexOf("?") !== -1 ? loginString.slice(0, loginString.indexOf("?")) : loginString);
+
+				var sequelizeObj = process.env.LOGDB ? {dialect: 'mysql'} : {dialect: 'mysql', logging: false};
+				var sequelize = new Sequelize(loginString, sequelizeObj);
+				generateTables(sequelize, forcesync, cb);
+
+				return mysqlTables;
+			};
+
+
+			//public Database
+			module.exports = mysqlTables;
