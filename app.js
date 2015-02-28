ea34e55f23dc7f1d99322d8f080701430e2fa5a6
Joao Diogo Falcao <john.diogo.m.falcao@gmail.com>
2015-02-28 13:48:07 -0800

    Initial DB, Keen, PostMark Set up

--- app.js
+++ app.js
@@ -1,14 +1,79 @@
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('baytek-analytics:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = process.env.PORT || '3000';
+
+
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var KeenIO = require('keen.io');
 
-var app = express();
+var DB = require('./models');
+
+// Define Environment Configuration
+var node_env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+
+// NEW RELIC PERFORMANCE METER
+if(node_env != 'development'){
+    require('newrelic');
+}
+
+
+    // Configure instance. Only projectId and writeKey are required to send data.
+    var keen = KeenIO.configure({
+        projectId: process.env['KEEN_PROJECT_ID'],
+        writeKey: process.env['KEEN_WRITE_KEY']
+    });
+
+
+    // Email Service
+    //var postmark = POSTMark(process.env.POSTMARK_API_KEY);
+    //////////////////////
+
+    // Login and Synchronize Database
+    var dbTables = DB.config(dbSynced);
+    /////////////////////////////////
+
+    function dbSynced(){
+
+        var app = express();
+
+        console.log('\u001b[33mMySql Database \u001b[31m' + process.env.CLEARDB_DATABASE_URL + '\u001b[33m synced.\033[0m');
+
+            // all environments
+            app.set('db', dbTables);
+            app.set('keen', keen);
+            // development only
+
+            app.use(expressSession({
+                secret: '<h1>YUUUUPPPIIII</h1>',
+                maxAge:  new Date() + 7200000,
+                key: 'sid',
+                cookie: {
+                    secret: true,
+                    expires: false
+                },
+                resave: false
+            }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,11 +84,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('sjdhf4;2dhGDas_56f;FDjk'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// MiddleWare
+// Pass DB to request
+app.use(function(req,res,next){
+    req.db = app.get('db');
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +130,119 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+
+        app.set('port', port);
+
+        /**
+         * Create HTTP server.
+         */
+
+        var server = http.createServer(app);
+
+        /**
+         * Listen on provided port, on all network interfaces.
+         */
+
+        server.listen(port);
+        server.on('error', onError);
+        server.on('listening', onListening);
+
+        /**
+         * Normalize a port into a number, string, or false.
+         */
+
+        function normalizePort(val) {
+            var port = parseInt(val, 10);
+
+            if (isNaN(port)) {
+                // named pipe
+                return val;
+            }
+
+            if (port >= 0) {
+                // port number
+                return port;
+            }
+
+            return false;
+        }
+
+        /**
+         * Event listener for HTTP server "error" event.
+         */
+
+        function onError(error) {
+            if (error.syscall !== 'listen') {
+                throw error;
+            }
+
+            var bind = typeof port === 'string'
+                ? 'Pipe ' + port
+                : 'Port ' + port
+
+            // handle specific listen errors with friendly messages
+            switch (error.code) {
+                case 'EACCES':
+                    console.error(bind + ' requires elevated privileges');
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    console.error(bind + ' is already in use');
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        }
+
+        /**
+         * Event listener for HTTP server "listening" event.
+         */
+
+        function onListening() {
+            var addr = server.address();
+            var bind = typeof addr === 'string'
+                ? 'pipe ' + addr
+                : 'port ' + addr.port;
+            debug('Listening on ' + bind);
+        }
+
+
+        /*      // Configure Router
+              //////////////////
+              // Main content
+              app.configure(function(){
+                  // Public App
+                  app.get('/', Routes.fbCrawler, Routes.publicApp);
+
+                  // Client App
+                  app.get('/bo', Routes.admin.checkAccess, Routes.privateApp);
+              });
+              // Login
+              app.configure(function(){
+                  app.get('/login', Routes.login);
+                  app.post('/api/login', Routes.admin.login);
+                  app.put('/api/logout', Routes.admin.logout);
+                  app.post('/api/forgot', Routes.admin.forgot);
+                  app.get('/confirm/email/:id/:hash', Routes.admin.confirmEmail);
+              });
+              // User
+              app.configure(function(){
+                  app.get('/api/users/:id', Routes.users.getOne);
+                  app.post('/api/users', Routes.admin.checkEmail, Routes.admin.register);
+                  app.put('/api/users/:id', Routes.admin.checkAccess, Routes.users.editOne);
+                  //app.del('/api/users/:id', Routes.checkUser, Routes.delUser);
+              });
+              // Approve widget
+              app.configure(function(){
+                  app.get('/api/pending', Routes.admin.checkAccess, Routes.pending.getAll);
+                  app.put('/api/pending/:id', Routes.admin.checkAccess, Routes.pending.denyOne);
+              });
+
+      */
+
+    }
+
+
